f9b62bac1f87e33ab5787388d99a0800d991513d
vinxent <1149536116@qq.com>
2016-01-12 01:11:05 +0800

    增加静态文件

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 var koa = require('koa');
+//var router = require('koa-router')();
 var app = koa();
+app.use(require('koa-static')('public'));
 
-app.use(function *() {
+// x-response-time
+app.use(function *(next) {
+    var start = new Date;
     console.log('some is coming');
+    yield next;
+    var ms = new Date - start;
+    this.set('X-Response-Time', ms + 'ms');
+});
+
+// logger
+app.use(function *(next) {
+    var start = new Date;
+    yield next;
+    var ms = new Date - start;
+    console.log('%s %s - %s', this.method, this.url, ms);
+});
+
+// response
+app.use(function *() {
     this.body = 'hello world';
 });
 
 app.listen(8080, function() {
-    console.log('server is listening');
+    console.log('server is listening', app.name, app.env, app.proxy);
+});
+
+app.on('error', function(err, ctx) {
+    console.log('server errer', err, ctx);
 });
---
+++ public/js/main.js
@@ -0,0 +1,3 @@
+/**
+ * Created by vinxent on 2016/1/12.
+ */
