de9d0af0399fbf0a0f2c0f966115a00bd9eb91fd
unknown <1149536116@qq.com>
2016-05-19 00:10:49 +0800

    总路由分发各个路由，向express看齐

--- router.js
+++ router.js
@@ -1,9 +1,19 @@
 'use strict';
 
+var Router = require('koa-router');
+
 module.exports = function (router) {
 
-    require('./src/routes/Home')(router);
-    require('./src/routes/User')(router);
+    var user = new Router();
+    var home = new Router();
+
+    require('./src/routes/User')(user);
+    require('./src/routes/Home')(home);
+
+    router.use('/user', user.routes(), user.allowedMethods());
+    router.use('/home', home.routes(), home.allowedMethods());
+
+
 
 };
 
