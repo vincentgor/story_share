38ec3f04943ca775b7d743ea8bcc8b459fcf42d5
unknown <1149536116@qq.com>
2016-05-25 00:56:53 +0800

    增加vue-router

--- public/js/vue.js
+++ public/js/vue.js
@@ -2,39 +2,68 @@
  * Created by vinxent on 2016/5/23.
  */
 
-new Vue({
-    el: '#koala',
-    data: {
-        show: false,
-        transitionName: 'fade'
-    }
+var Foo = Vue.extend({
+    template:
+        '<div class="foo">' +
+        '<p>This is Foo!</p>' +
+        '<router-view></router-view>' + // <- 嵌套的外链
+        '</div>'
 });
 
-Vue.transition('expand', {
+var Baz = Vue.extend({
+    template:  '<p>This is Baz!</p>'
+});
 
-    beforeEnter: function (el) {
-        el.textContent = 'beforeEnter'
-    },
-    enter: function (el) {
-        el.textContent = 'enter'
-    },
-    afterEnter: function (el) {
-        el.textContent = 'afterEnter'
-    },
-    enterCancelled: function (el) {
-        // handle cancellation
-    },
+var Bar = Vue.extend({
+    template:  '<p>This is Bar!</p>'
+});
 
-    beforeLeave: function (el) {
-        el.textContent = 'beforeLeave'
-    },
-    leave: function (el) {
-        el.textContent = 'leave'
-    },
-    afterLeave: function (el) {
-        el.textContent = 'afterLeave'
-    },
-    leaveCancelled: function (el) {
-        // handle cancellation
+var router = new VueRouter({
+    root: '/foo'
+});
+
+
+
+router.map({
+
+    '/user/:userId': {
+        name: 'user',
+        component: {
+            template: '<p>shabi</p>'
+        }
     }
-})
+
+//    '/foo': {
+//        component: Foo,
+//        subRoutes: {
+//            '/': {
+//                component: {
+//                    template: '<p>Default sub view for Foo</p>'
+//                }
+//            },
+//            '/user/:username': {
+//                component: {
+//                    template: '<p>用户名是{{$route.params.username}}</p>'
+//                }
+//            },
+//            '/bar': {
+//                component: Bar
+//            }
+//        }
+//    }
+});
+
+//router.beforeEach((transition) => {
+//    return transition;
+////    if (transition.to.auth) {
+////        console.log('需要授权');
+////    } else {
+////        console.log('不需要授权');
+////    }
+//});
+
+router.go({ name: 'user', params: { userId: 123 }});
+
+var App = Vue.extend({});
+
+router.start(App, '#app');
