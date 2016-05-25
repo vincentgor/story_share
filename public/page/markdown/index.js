6a896d8ba021a5428268878140536199fabf7adb
unknown <1149536116@qq.com>
2016-05-25 23:30:05 +0800

    add markdown

---
+++ public/page/markdown/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by vinxent on 2016/5/25.
+ */
+
+new Vue({
+    el: '#editor',
+    data: {
+        input: '# hello'
+    }
+//    filters: {
+//        marked: false
+//    }
+});
