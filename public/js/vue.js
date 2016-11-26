e752d6a46e7f4342b403435bf53656fd4e9c7d84
root <root@vinxent.(none)>
2016-11-27 01:03:24 +0800

    update

--- public/js/vue.js
+++ public/js/vue.js
@@ -1,69 +1,64 @@
-/**
- * Created by vinxent on 2016/5/23.
- */
 
-var Foo = Vue.extend({
-    template:
-        '<div class="foo">' +
-        '<p>This is Foo!</p>' +
-        '<router-view></router-view>' + // <- 嵌套的外链
-        '</div>'
-});
+var User = {
+  template: '<div class="user">' +
+      '<h2>User {{ $route.params.id }}</h2>' +
+      '<router-view></router-view>' +
+    '</div>'
 
-var Baz = Vue.extend({
-    template:  '<p>This is Baz!</p>'
-});
+}
 
-var Bar = Vue.extend({
-    template:  '<p>This is Bar!</p>'
-});
+var UserHome = { template: '<div>Home</div>' }
+var UserProfile = { template: '<div>Profile</div>' }
+var UserPosts = { template: '<div>Posts</div>' }
+
+
+var routes = [
+    // 动态路径参数 以冒号开头
+      { 
+        path: '/user/:id', 
+        component: User,
+        children: [
+          {
+            path: '',
+            component: UserHome
+          },
+          {
+            path: 'profile',
+            component: UserProfile
+          },
+          {
+            path: 'post',
+            component: UserPosts
+          },
+          
+        ]
+      }
+  ]
 
 var router = new VueRouter({
-    root: '/foo'
-});
+    // mode: 'history',
+    routes:routes
+})
 
+var app = new Vue({
+  router:router
+}).$mount('#app');
 
-
-router.map({
-
-    '/user/:userId': {
-        name: 'user',
-        component: {
-            template: '<p>shabi</p>'
-        }
-    }
-
-//    '/foo': {
-//        component: Foo,
-//        subRoutes: {
-//            '/': {
-//                component: {
-//                    template: '<p>Default sub view for Foo</p>'
-//                }
-//            },
-//            '/user/:username': {
-//                component: {
-//                    template: '<p>用户名是{{$route.params.username}}</p>'
-//                }
-//            },
-//            '/bar': {
-//                component: Bar
-//            }
-//        }
-//    }
-});
-
-//router.beforeEach((transition) => {
-//    return transition;
-////    if (transition.to.auth) {
-////        console.log('需要授权');
-////    } else {
-////        console.log('不需要授权');
-////    }
-//});
-
-router.go({ name: 'user', params: { userId: 123 }});
-
-var App = Vue.extend({});
-
-router.start(App, '#app');
+var app_ = new Vue({
+  data: {
+    datelist: [
+      {time: 'March 2014'},
+      {time: 'February 2014'},
+      {time: 'January 2014'},
+      {time: 'December 2013'},
+      {time: 'November 2013'},
+      {time: 'October 2013'},
+      {time: 'September 2013'},
+      {time: 'August 2013'},
+      {time: 'July 2013'},
+      {time: 'June 2013'},
+      {time: 'May 2013'},
+      {time: 'April 2013'}
+    ]
+  }
+}).$mount('#datelist');
